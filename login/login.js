0ea6959791994b8546d8525369c670ed058e193c
Aleix Prat Marin <aleixpratmarin@gmail.com>
2024-02-13 12:42:24 +0100

    login

--- login/login.js
+++ login/login.js
@@ -1,41 +1,50 @@
 function validateAndSubmit() {
-    // Obtenir les dades del formulari
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-  
-    // Comprovar si els camps estan buits
-    if (username.trim() === '' || password.trim() === '') {
-      alert('Completa tots els camps.');
-      return;
-    }
-  
-    // Comprovar el format del nom d'usuari (potser voldràs afegir més comprovacions)
-    if (!isValidUsername(username)) {
-      alert('Nom d\'usuari no vàlid.');
-      return;
-    }
-  
-    // Comprovar el format de la contrasenya (potser voldràs afegir més comprovacions)
-    if (!isValidPassword(password)) {
-      alert('Contrasenya no vàlida.');
-      return;
-    }
-  
-    // Si tot està bé, envia les dades al servidor per a l'autenticació
-    // Aquí pots fer servir AJAX per enviar les dades al servidor
-    // Exemple fictici amb l'ús de la consola per mostrar el resultat
-    console.log('Dades enviades al servidor:', { username, password });
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  if (username.trim() === '' || password.trim() === '') {
+    alert('Completa tots els camps.');
+    return;
   }
-  
-  function isValidUsername(username) {
-    // Aquí pots afegir lògica per validar el format del nom d'usuari
-    // Retornaràs true si és vàlid, false si no ho és
-    return true;
+
+  if (!isValidUsername(username)) {
+    alert('Nom d\'usuari no vàlid.');
+    return;
   }
-  
-  function isValidPassword(password) {
-    // Aquí pots afegir lògica per validar el format de la contrasenya
-    // Retornaràs true si és vàlida, false si no ho és
-    return true;
+
+  if (!isValidPassword(password)) {
+    alert('Contrasenya no vàlida.');
+    return;
   }
-  
+
+  var url = 'beatify/usuari'; // Canvia a la teva URL del servidor
+  var data = { username: username, password: password };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(response => handleServerResponse(response))
+    .catch(error => console.error('Error de la crida Fetch:', error));
+}
+
+function isValidUsername(username) {
+  return username.length >= 3;
+}
+
+function isValidPassword(password) {
+  return password.length >= 6;
+}
+
+function handleServerResponse(response) {
+  if (response.success) {
+    alert(response.message);
+    // Redirigeix l'usuari o realitza altres accions desitjades després de l'inici de sessió
+  } else {
+    alert('Error d\'inici de sessió: ' + response.message);
+  }
+}
