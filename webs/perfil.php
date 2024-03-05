4fdc59e7dbd775b5600db3cd07f5b65b9849742b
Aleix Prat Marin <aleixpratmarin@gmail.com>
2024-03-05 10:39:23 +0100

    asdas

--- webs/perfil.php
+++ webs/perfil.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/js/perfil.css">
+    <link rel="stylesheet" href="../assets/css/perfil.css">
     <link rel="icon" href="../img/Logo_sense_fons.png">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200&display=swap" rel="stylesheet">
