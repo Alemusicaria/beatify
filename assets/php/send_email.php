d1a9355b56301382647efb598272137a62f9dbfc
Omega15RD <angelelpidiocespedes9@gmail.com>
2024-03-11 18:03:02 +0100

    ffff

--- assets/php/send_email.php
+++ assets/php/send_email.php
@@ -1,19 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST["name"];
-    $telefono = $_POST["telefono"];
-    $email = $_POST["email"];
-    $assumpte = $_POST["assumpte"];
-    $mensaje = $_POST["textarea"];
+    $nombre = isset($_POST["name"]) ? $_POST["name"] : "";
+    $telefono = isset($_POST["telefono"]) ? $_POST["telefono"] : "";
+    $email = isset($_POST["email"]) ? $_POST["email"] : "";
+    $assumpte = isset($_POST["assumpte"]) ? $_POST["assumpte"] : "";
+    $mensaje = isset($_POST["textarea"]) ? $_POST["textarea"] : "";
 
-    $to = "angelelpidiocespedes9@gmail.com"; // Reemplaza con la dirección de correo a la que deseas enviar el formulario
+    $to = "tu@email.com";
     $subject = "Nuevo mensaje desde el formulario de contacto";
     $message = "Nombre: $nombre\nTeléfono: $telefono\nEmail: $email\nAsunto: $assumpte\nMensaje: $mensaje";
-
-    // Puedes personalizar el encabezado del correo según tus necesidades
     $headers = "From: $email";
 
-    // Envía el correo
     mail($to, $subject, $message, $headers);
 }
 ?>
