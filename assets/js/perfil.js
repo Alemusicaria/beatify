fa84cd94b0a01db6cc0a786f119d4a3ff2f4fd9c
Aleix Prat <aleixpratmarin@gmail.com>
2024-03-14 09:32:49 +0100

    dfdsf

--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -1,3 +1,4 @@
+// Funció per desar la personalització
 function guardarPersonalizacion() {
     const opciones = {
         forma: document.getElementById('button-shape').value,
@@ -8,24 +9,27 @@ function guardarPersonalizacion() {
     document.cookie = `personalizacion=${JSON.stringify(opciones)}; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/`;
 
     document.getElementById('customize-options').classList.add('hidden');
-  
 }
 
+// Mostra les opcions de personalització quan es fa clic
 document.getElementById('btn-customize').addEventListener('click', () => {
     document.getElementById('customize-options').classList.remove('hidden');
 });
 
-document.getElementById('btn-save').addEventListener('click', guardarPersonalizacion);
+// Quan el botó de desar es fa clic, executa la funció per desar la personalització
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btn-save').addEventListener('click', guardarPersonalizacion);
 
-// PERFIL
-$(document).ready(function () {
+    // Amaga la llista desplegable al començament
     $('.dropdown-list').hide();
-
 });
+
+// Funció per alternar la visualització de la llista desplegable
 function toggleDropdown() {
     $('.dropdown-list').toggle();
 }
 
+// Amaga la llista desplegable quan es fa clic fora d'ella
 $(document).on('click', function (e) {
     if (!$(e.target).closest('.perfil-dropdown').length) {
         $('.dropdown-list').hide();
