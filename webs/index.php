b1e8fec7e74704fcfb355b1e4ef97dfa23f86912
Aleix Prat Marin <aleixpratmarin@gmail.com>
2024-04-09 10:57:57 +0200

    fdsf

--- webs/index.php
+++ webs/index.php
@@ -15,6 +15,45 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Beatify</title>
     <script src="../assets/js/dadesUser.js"></script>
+    <script>
+        // Definir la función toggleDropdown fuera de $(document).ready()
+        function toggleDropdown() {
+            $('.dropdown-list').toggle();
+        }
+
+        $(document).ready(function() {
+            // Mostrar opciones de personalización al hacer clic en el botón
+            $('#btn-customize').click(function() {
+                $('#customize-options').removeClass('hidden');
+            });
+
+            // Guardar personalización al hacer clic en el botón de guardar
+            $('#btn-save').click(guardarPersonalizacion);
+
+            // Ocultar la lista desplegable al inicio
+            $('.dropdown-list').hide();
+
+            // Ocultar la lista desplegable al hacer clic fuera de ella
+            $(document).click(function(e) {
+                if (!$(e.target).closest('.perfil-dropdown').length) {
+                    $('.dropdown-list').hide();
+                }
+            });
+        });
+
+        // Función para guardar la personalización
+        function guardarPersonalizacion() {
+            const opciones = {
+                forma: $('#button-shape').val(),
+                color: $('#color-picker').val(),
+                fontSize: $('#font-size').val(),
+            };
+
+            document.cookie = `personalizacion=${JSON.stringify(opciones)}; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/`;
+
+            $('#customize-options').addClass('hidden');
+        }
+    </script>
 </head>
 
 <body>
