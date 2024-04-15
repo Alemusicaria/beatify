32e7002aa01b7e56994be804e2194a21f567bae4
Omega15RD <angelelpidiocespedes9@gmail.com>
2024-04-15 17:46:38 +0200

    meh

--- assets/php/obtainInfArtist.php
+++ assets/php/obtainInfArtist.php
@@ -1,35 +1,36 @@
 <?php
- $servername = "localhost";
- $dbusername = "root";
- $dbpassword = "";
- $dbname = "Beatify";
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "Beatify";
 
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
 if (isset($_POST['selectedArtist'])) {
-    $data=$_POST['selectedArtist'];
+    $data = $_POST['selectedArtist'];
 
-    $sql = "SELECT Artista.ID AS ID_Artista, Artista.Info, Canco.Titol AS TitolCanco, Album.Titol AS TitolAlbum
+    $sql = "SELECT Artista.ID AS ID_Artista, Artista.Info, Album.Titol AS TitolAlbum, GROUP_CONCAT(Canco.Titol) AS Canciones
     FROM Artista
-    INNER JOIN Album  ON Artista.ID = Album.ID_Artista
+    INNER JOIN Album ON Artista.ID = Album.ID_Artista
     INNER JOIN Crea_musica ON Artista.ID = Crea_musica.ID_Artista
     LEFT JOIN Canco ON Crea_musica.ID_Canco = Canco.ID
-    WHERE Artista.NomArtistic = '$data'";
+    WHERE Artista.NomArtistic = '$data'
+    GROUP BY Artista.ID, Album.Titol";
 
     $result = $conn->query($sql);
 
     if ($result && $result->num_rows > 0) {
         $array = array();
         while ($row = $result->fetch_assoc()) {
-                $array[] = array(
-                    'TitolCanco' => $row['TitolCanco'],
-                    'TitolAlbum' => $row['TitolAlbum'],
-                    'Info' => $row['Info']
-                ); 
+            $array[] = array(
+                'TitolAlbum' => $row['TitolAlbum'],
+                'Info' => $row['Info'],
+                'Canciones' => explode(',', $row['Canciones'])
+            );
         }
-        $array = array_values($array);
         echo json_encode($array);
     } else {
         echo json_encode(array('error' => 'No se encontraron canciones en la base de datos'));
     }
 }
+?>
