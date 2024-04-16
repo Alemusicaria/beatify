941714aa0ef4d18ddc8e6721a7a0ef1acbf1388a
Angel Cespedes <angelelpidiocespedes9@gmail.com>
2024-04-16 11:11:06 +0200

    ccc

--- assets/js/infArtistes.js
+++ assets/js/infArtistes.js
@@ -18,12 +18,30 @@ document.addEventListener('DOMContentLoaded', function () {
     function carregarArtista(artista) {
         if (selectedArtist) {
             $('.foto img').attr('src', "../musica/artistes/" + selectedArtist + ".jpg");
-        
+
             $('.txt h2').text(selectedArtist);
 
             $('.artista h3').text(artista.Info);
-        
-           
         }
+        crearTaules(artista);
+    }
+    function crearTaules(artista) {
+        var tablaAlbums = document.getElementById("tablaAlbums");
+        tablaAlbums.innerHTML = "";
+        artista.canciones.forEach(function (cancion) {
+            cancion.Albums.forEach(function (album) {
+                var albumElemento = document.createElement("div");
+                albumElemento.textContent = album;
+                tablaAlbums.appendChild(albumElemento);
+            });
+        });
+
+        var tablaCanciones = document.getElementById("tablaCanciones");
+        tablaCanciones.innerHTML = "";
+        artista.canciones.forEach(function (cancion) {
+            var cancionElemento = document.createElement("div");
+            cancionElemento.textContent = cancion.TitolCanco;
+            tablaCanciones.appendChild(cancionElemento);
+        });
     }
 });
