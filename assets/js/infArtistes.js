99127a4a0f133cc5f2a68a1ac0bdd888e2962fc6
Angel Cespedes <angelelpidiocespedes9@gmail.com>
2024-04-16 11:17:45 +0200

    cc

--- assets/js/infArtistes.js
+++ assets/js/infArtistes.js
@@ -9,36 +9,28 @@ document.addEventListener('DOMContentLoaded', function () {
         success: function (response) {
             console.log(response);
             let artista = JSON.parse(response);
-            carregarArtista(artista);
+            mostrarInformacionArtista(artista);
         },
         error: function (error) {
             console.log('Error en obtener las canciones:', error);
         }
     });
-    function carregarArtista(artista) {
-        if (selectedArtist) {
-            $('.foto img').attr('src', "../musica/artistes/" + selectedArtist + ".jpg");
-
-            $('.txt h2').text(selectedArtist);
-
-            $('.artista h3').text(artista.Info);
-        }
-        crearTaules(artista);
-    }
-    function crearTaules(artista) {
+    function mostrarInformacionArtista(artista) {
+        // Mostrar los álbumes
         var tablaAlbums = document.getElementById("tablaAlbums");
         tablaAlbums.innerHTML = "";
-        artista.canciones.forEach(function (cancion) {
-            cancion.Albums.forEach(function (album) {
+        artista.canciones.forEach(function(cancion) {
+            cancion.Albums.forEach(function(album) {
                 var albumElemento = document.createElement("div");
                 albumElemento.textContent = album;
                 tablaAlbums.appendChild(albumElemento);
             });
         });
-
+    
+        // Mostrar las canciones
         var tablaCanciones = document.getElementById("tablaCanciones");
         tablaCanciones.innerHTML = "";
-        artista.canciones.forEach(function (cancion) {
+        artista.canciones.forEach(function(cancion) {
             var cancionElemento = document.createElement("div");
             cancionElemento.textContent = cancion.TitolCanco;
             tablaCanciones.appendChild(cancionElemento);
