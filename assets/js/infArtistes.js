7b90247dafb8a55c5708d85af5254ddfb828401a
Omega15RD <angelelpidiocespedes9@gmail.com>
2024-04-16 16:53:29 +0200

    ddd

--- assets/js/infArtistes.js
+++ assets/js/infArtistes.js
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log('Error en obtener las canciones:', error);
         }
     });
-    function carregarArtista(artista){
+    function carregarArtista(artista) {
         $('.foto img').attr('src', "../musica/artistes/" + selectedArtist + ".jpg");
-    
-        
+
+
         $('.txt h2').text(artista.NomArtistic);
-        
+
         // Limpiamos el contenido actual de la etiqueta '.artista' antes de agregar los nuevos artistas
         $('.artista h3').text(artista.Info);
         mostrarInformacionArtista(artista)
@@ -30,29 +30,42 @@ document.addEventListener('DOMContentLoaded', function () {
         // Mostrar los álbumes
         var tablaAlbums = document.getElementById("tablaAlbums");
         tablaAlbums.innerHTML = "";
-    
-        Object.keys(artista.canciones).forEach(function(key) {
+
+        var albumsYaAgregados = new Set(); // Conjunto para rastrear álbumes
+
+        Object.keys(artista.canciones).forEach(function (key) {
             var cancion = artista.canciones[key];
-            cancion.Albums.forEach(function(album) {
-                var listAlbumDiv = document.createElement("div");
-                listAlbumDiv.classList.add("listAlbum");
-                var albumParagraph = document.createElement("p");
-                albumParagraph.textContent = album;
-                listAlbumDiv.appendChild(albumParagraph);
-                tablaAlbums.appendChild(listAlbumDiv);
+            cancion.Albums.forEach(function (album) {
+                if (!albumsYaAgregados.has(album)) {
+                    var listAlbumDiv = document.createElement("div");
+                    listAlbumDiv.classList.add("listAlbum");
+                    var divPortadaDiv = document.createElement("div");
+                    divPortadaDiv.classList.add("divPortada");
+                    var imgPortada = document.createElement("img");
+                    imgPortada.classList.add("portadaList");
+                    imgPortada.src = '../musica/portades/' + album + ".jpg";
+                    divPortadaDiv.appendChild(imgPortada);
+                    var albumParagraph = document.createElement("p");
+                    albumParagraph.textContent = album;
+                    listAlbumDiv.appendChild(divPortadaDiv);
+                    listAlbumDiv.appendChild(albumParagraph);
+                    tablaAlbums.appendChild(listAlbumDiv);
+                    albumsYaAgregados.add(album);
+                }
             });
         });
-    
+
+
         // Mostrar las canciones
         var tablaCanciones = document.getElementById("tablaCanciones");
         tablaCanciones.innerHTML = "";
-    
-        Object.keys(artista.canciones).forEach(function(key) {
+
+        Object.keys(artista.canciones).forEach(function (key) {
             var cancion = artista.canciones[key];
-            
+
             var listSongsDiv = document.createElement("div");
             listSongsDiv.classList.add("listSongs");
-    
+
             var divPortadaDiv = document.createElement("div");
             divPortadaDiv.classList.add("divPortada");
             var imgPortada = document.createElement("img");
@@ -60,17 +73,17 @@ document.addEventListener('DOMContentLoaded', function () {
             // Si hay un álbum asociado, usamos su nombre para buscar la imagen, de lo contrario, usamos el título de la canción
             imgPortada.src = '../musica/portades/' + (cancion.Albums.length > 0 ? cancion.Albums[0] : cancion.TitolCanco) + ".jpg";
             divPortadaDiv.appendChild(imgPortada);
-    
+
             var divCancoDiv = document.createElement("div");
             divCancoDiv.classList.add("divCanco");
             var h4Element = document.createElement("h4");
             h4Element.textContent = cancion.TitolCanco;
             divCancoDiv.appendChild(h4Element);
-    
+
             listSongsDiv.appendChild(divPortadaDiv);
             listSongsDiv.appendChild(divCancoDiv);
             tablaCanciones.appendChild(listSongsDiv);
         });
     }
-    
+
 });
