3b2d0816a7e3a9b1a26830d7fa08d47eb82accf4
Angel Cespedes <angelelpidiocespedes9@gmail.com>
2024-04-29 11:33:16 +0200

    zzzz

--- assets/js/code.js
+++ assets/js/code.js
@@ -5,6 +5,8 @@ const playPause = document.getElementById('PlayPause');
 const plus10 = document.getElementById('Plus10');
 const back10 = document.getElementById('Back10');
 const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-bar');
+const progressBall = document.querySelector('.progress-ball');
 
 const calculateTime = (secs) => {
     const minutes = Math.floor(secs / 60);
@@ -17,9 +19,16 @@ const displayDuration = () => {
     songLength.innerHTML = calculateTime(audio.duration);
 };
 
-const setProgress = () => {
-    let percentage = (audio.currentTime / audio.duration) * 100;
+const setProgress = (percentage) => {
     progress.style.width = percentage + '%';
+    progressBall.style.left = percentage + '%'; // Ajuste para mover la bola junto con la barra de progreso
+};
+
+const updateProgress = (e) => {
+    const clickX = e.offsetX;
+    const width = progressContainer.clientWidth;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
 };
 
 const togglePlayPause = () => {
@@ -55,11 +64,13 @@ audio.addEventListener('loadedmetadata', () => {
 
 audio.addEventListener('timeupdate', () => {
     currentTime.innerHTML = calculateTime(audio.currentTime);
-    setProgress();
+    const percentage = (audio.currentTime / audio.duration) * 100;
+    setProgress(percentage);
 });
 
-// Inicialización
-if (audio.readyState > 0) {
-    displayDuration();
-    currentTime.innerHTML = calculateTime(audio.currentTime);
-}
+progressContainer.addEventListener('click', updateProgress);
+progressContainer.addEventListener('mousemove', (e) => {
+    if (e.buttons === 1) {
+        updateProgress(e);
+    }
+});
