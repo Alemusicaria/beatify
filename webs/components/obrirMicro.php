7bb166654747bc56bf678f0b2db5a786ec7ef56a
Salahaden el majjati <salahadenem.smx2@gmail.com>
2024-04-30 13:11:49 +0200

    gh

--- webs/components/obrirMicro.php
+++ webs/components/obrirMicro.php
@@ -2,3 +2,6 @@
     <div id="inputTextToSave" style="overflow: scroll; height: 200px"></div>
     <script src="js.js"></script>
 </div>
+<link rel="stylesheet" href="../assets/dist/css/bootstrap.min.css">
+<script src="../assets/dist/js/jquery.min.js"></script>
+<script src="../assets/dist/js/bootstrap.min.js"></script>
