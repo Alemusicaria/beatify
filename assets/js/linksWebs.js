a492d82317c3fefa72eb82bc4d2c03f4946a4940
Omega15RD <angelelpidiocespedes9@gmail.com>
2024-05-06 17:10:22 +0200

    www

--- assets/js/linksWebs.js
+++ assets/js/linksWebs.js
@@ -10,6 +10,9 @@ $(document).ready(function () {
         });
     }
 
+    // Llamada inicial al cargar la página
+    cargarInici();
+
     // Evento de clic para el elemento #crear
     $('#crear').click(function () {
         $.ajax({
