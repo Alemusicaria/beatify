991bc14b026d4dc7bfd9b36ef00c397f219dcb8a
Aleix Prat Marin <aleixpratmarin@gmail.com>
2024-05-14 11:15:40 +0200

    asdfasdf

--- assets/js/linksWebs.js
+++ assets/js/linksWebs.js
@@ -1,78 +1,88 @@
 $(document).ready(function () {
-    // Función para realizar la llamada AJAX a ./components/inici.php
+    // Funció per carregar la pàgina d'inici al carregar la pàgina
     function cargarInici() {
+        // Sol·licitud AJAX per obtenir el contingut de 'inici.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/inici.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     }
 
-    // Llamada inicial al cargar la página
+    // Llamada inicial per carregar la pàgina d'inici
     cargarInici();
 
-    // Evento de clic para el elemento #crear
+    // Esdeveniment de clic per l'element #crear
     $('#crear').click(function () {
+        // Sol·licitud AJAX per obtenir el contingut de 'crearLlista.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/crearLlista.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     });
 
-    // Evento de clic para el elemento #asistencia
+    // Esdeveniment de clic per l'element #asistencia
     $('#asistencia').click(function () {
+        // Sol·licitud AJAX per obtenir el contingut de 'asistencia.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/asistencia.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     });
 
-    // Evento de clic para el elemento #inici
+    // Esdeveniment de clic per l'element #inici
     $('#inici').click(function () {
-        // Llamar a la función cargarInici en lugar de definir nuevamente la llamada AJAX
+        // Crida a la funció 'cargarInici()' per recarregar la pàgina d'inici
         cargarInici();
     });
 
-    // Evento de clic para el elemento #obrirMicro
+    // Esdeveniment de clic per l'element #obrirMicro
     $('#obrirMicro').click(function () {
+        // Sol·licitud AJAX per obtenir el contingut de 'obrirMicro.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/obrirMicro.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     });
 
-    // Evento de clic para el elemento #obrirMicro
+    // Esdeveniment de clic per l'element #lletra
     $('#lletra').click(function () {
+        // Sol·licitud AJAX per obtenir el contingut de 'lletra.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/lletra.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     });
 
+    // Esdeveniment de clic per l'element #premium
     $('#premium').click(function () {
+        // Sol·licitud AJAX per obtenir el contingut de 'premium.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/premium.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
-    });    
+    });
+
+    // Esdeveniment de clic per l'element .portada (manejat mitjançant delegació d'esdeveniments)
     $(document).on('click', '.portada', function () {
+        // S'obtenen les dades de la cançó seleccionada i es guarden a la memòria local
         var songTitle = $(this).siblings('h4').text();
         var artistInfo = $(this).siblings('p').text();
         var imgSrc = $(this).attr('src');
@@ -81,32 +91,46 @@ $(document).ready(function () {
             artistInfo: artistInfo,
             imgSrc: imgSrc
         }));
+        // Es crida a la funció per carregar la pàgina de cançons
         pageSongs();
     });
+
+    // Funció per carregar la pàgina de cançons
     function pageSongs() {
+        // Sol·licitud AJAX per obtenir el contingut de 'pageSongs.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/pageSongs.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     }
+
+    // Esdeveniment de clic per l'element .nom-artista (manejat mitjançant delegació d'esdeveniments)
     $(document).on('click', '.nom-artista', function () {
+        // S'obté el nom de l'artista seleccionat i es guarda a la memòria local
         var nombreArtista = $(this).text();
         localStorage.setItem('selectedArtist', nombreArtista);
+        // Es crida a la funció per carregar la pàgina d'artistes
         pageArtistas();
     });
+
+    // Funció per carregar la pàgina d'artistes
     function pageArtistas() {
+        // Sol·licitud AJAX per obtenir el contingut de 'pageArtistas.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/pageArtistas.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     }
+
+    // Esdeveniment de clic per l'element .boxLlista (manejat mitjançant delegació d'esdeveniments)
     $(document).on('click', '.boxLlista', function () {
+        // S'obtenen les dades de la llista seleccionada i es guarden a la memòria local
         var nomLlista = $(this).children('a').text();
         var idLlista = $(this).children('p#lista').text();
         var idUser = $(this).children('p#user').text();
@@ -117,33 +141,41 @@ $(document).ready(function () {
             id_User: idUser,
             img: img
         }));
+        // Es crida a la funció per carregar la pàgina de llistes
         pageListas();
     });
+
+    // Funció per carregar la pàgina de llistes
     function pageListas() {
+        // Sol·licitud AJAX per obtenir el contingut de 'pageListas.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/pageListas.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     }
+
+    // Esdeveniment de clic per l'element .moreSongs (manejat mitjançant delegació d'esdeveniments)
     $(document).on('click', '.moreSongs', function () {
+        // S'obté la llista seleccionada des de la memòria local i es guarda a les cookies
         var selectedList = JSON.parse(localStorage.getItem('selectedList'));
         var idLlista = selectedList.id_Llista;
         document.cookie = "ID_llista=" + idLlista + "; path=/";
+        // Es crida a la funció per afegir més cançons
         addSongs();
     });
-    function addSongs(){
+
+    // Funció per afegir més cançons
+    function addSongs() {
+        // Sol·licitud AJAX per obtenir el contingut de 'addSongs.php' i inserir-lo a la pàgina
         $.ajax({
             url: './components/addSongs.php',
             type: 'GET',
             success: function (data) {
-                $('.contenedor-right').html(data);
+                $('.contenedor-right').html(data); // Insereix el contingut obtingut dins de l'element .contenedor-right
             }
         });
     }
 });
-
-
-
--- assets/js/llistaCanco.js
+++ assets/js/llistaCanco.js
@@ -1,39 +1,42 @@
+// Afegir un esdeveniment de clic al botó 'crearListaBtn'
 document.getElementById('crearListaBtn').addEventListener('click', function () {
+    // Obté el valor del camp d'entrada per al nom de la llista
     var nombreLista = document.getElementById('nomLlista').value;
 
-    // Comprobar si el nombre de la lista no está vacío
+    // Comprova si el nom de la llista no està buit
     if (nombreLista.trim() !== '') {
-        // Datos del formulario
+        // Dades del formulari a enviar
         var formData = new FormData();
         formData.append('nomLlista', nombreLista);
 
-        // Enviar solicitud AJAX
+        // Envia una sol·licitud AJAX utilitzant XMLHttpRequest
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '../assets/php/guardarLlista.php', true);
         xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 400) {
-                // Éxito
+                // Éxit de la sol·licitud AJAX
                 console.log(xhr.responseText);
-                // Aquí puedes realizar acciones adicionales después de la inserción, si es necesario
+                // Aquí es poden realitzar accions addicionals després de la inserció, si és necessari
             } else {
-                // Error de servidor
-                console.error('Error en la solicitud AJAX: ' + xhr.status);
+                // Error del servidor
+                console.error('Error en la sol·licitud AJAX: ' + xhr.status);
             }
         };
         xhr.onerror = function () {
-            // Error de conexión
-            console.error('Error de conexión');
+            // Error de connexió
+            console.error('Error de connexió');
         };
         xhr.send(formData);
 
-        // Ocultar el formulario después de enviar los datos
+        // Oculta el formulari després d'enviar les dades
         document.getElementById('formulario').style.display = 'none';
 
-        // Actualizar y mostrar el título con el nombre de la lista
+        // Actualitza i mostra el títol amb el nom de la llista
         var titulo = document.getElementById('nombreListaTitulo');
         titulo.textContent = nombreLista;
         titulo.style.display = 'block';
     } else {
+        // Mostre una alerta si el camp del nom de la llista està buit
         alert('Si us plau, introduïu un nom per a la llista.');
     }
 });
